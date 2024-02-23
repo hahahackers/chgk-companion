666d242ac122aeba650a1b8de4c1a27c89295f67
Alexey Selivanov <lazypacman@gmail.com>
2024-02-23 23:46:11 +0400

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,192 @@
-import Image from "next/image";
+//@ts-nocheck
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+const SECONDS = 60;
 
 export default function Home() {
+  const [state, setState] = useState('idle');
+  const [value, setValue] = useState('');
+
+  const [questions, setQuestions] = useState([]);
+  const [current, setCurrent] = useState(0);
+
+  async function handleClick() {
+    const response = await fetch(
+      `http://www.db.chgk.info/tours/${value}/questions?page=1&itemsPerPage=100`
+    );
+
+    const json = await response.json();
+
+    setQuestions(json['hydra:member']);
+
+    setState('loaded');
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="">
+      {state === 'idle' && (
+        <div className="flex-col inline-flex gap-2">
+          <label className="flex flex-col gap-2" htmlFor="">
+            Input package id
+            <input
+              className="border-2 border-blue-400 p-2 rounded"
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
             />
-          </a>
+          </label>
+          <button className="bg-slate-200 p-2 rounded" onClick={handleClick}>
+            Submit
+          </button>
         </div>
-      </div>
+      )}
+      {state === 'loaded' && (
+        <div className="inline-flex flex-col gap-2">
+          <ul className="flex gap-1">
+            {questions.map((q, i) => (
+              <li
+                key={i}
+                className={`flex-1 flex  justify-center rounded ${
+                  q.answered == null
+                    ? 'bg-slate-200'
+                    : q.answered
+                    ? 'bg-green-200'
+                    : 'bg-red-200'
+                }`}
+              >
+                <button onClick={() => setCurrent(i)}>{i + 1}</button>
+              </li>
+            ))}
+          </ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="mt-2">
+            {current + 1} of {questions.length}
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Question
+            question={questions[current]}
+            onAnswer={(answer: boolean) => {
+              setQuestions((questions) => {
+                questions[current].answered = answer;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                return [...questions];
+              });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              setCurrent((p) => Math.min(questions.length - 1, p + 1));
+            }}
+          >
+            <div className="flex gap-2 ">
+              <button
+                className="rounded bg-orange-200 p-2 disabled:bg-slate-200"
+                disabled={current === 0}
+                onClick={() => setCurrent((p) => Math.max(0, p - 1))}
+              >
+                Previous question
+              </button>
+              <button
+                className="rounded bg-orange-200 p-2 disabled:bg-slate-200"
+                disabled={current === questions.length - 1}
+                onClick={() =>
+                  setCurrent((p) => Math.min(questions.length - 1, p + 1))
+                }
+              >
+                Next question
+              </button>
+            </div>
+          </Question>
+        </div>
+      )}
     </main>
+  );
+}
+
+enum QuestionState {
+  Preparing,
+  TimerSet,
+  TimerExpired,
+  AnswerShown,
+}
+
+function Question({ question, onAnswer, children }) {
+  const [state, setState] = useState(QuestionState.Preparing);
+  const [elapsed, setElapsed] = useState(SECONDS);
+  const timer = useRef<number>();
+
+  useEffect(() => {
+    setState(QuestionState.Preparing);
+    setElapsed(SECONDS);
+
+    return () => {
+      timer.current && clearInterval(timer.current);
+    };
+  }, [question]);
+
+  useEffect(() => {
+    if (state === QuestionState.TimerSet && elapsed === 0) {
+      setState(QuestionState.TimerExpired);
+    }
+  }, [state, elapsed]);
+
+  function handleRunTimerClick() {
+    setState(QuestionState.TimerSet);
+
+    timer.current = setInterval(() => {
+      setElapsed((p) => p - 1);
+    }, 1000);
+  }
+
+  return (
+    <div>
+      <h1 className="text-2xl mb-6">{question.question}</h1>
+      <div className="flex justify-between">
+        <div className="inline-flex flex-col gap-2">
+          {state === QuestionState.Preparing && (
+            <button
+              className="rounded bg-blue-200 p-2"
+              onClick={handleRunTimerClick}
+            >
+              Run timer
+            </button>
+          )}
+
+          {state === QuestionState.TimerSet && <span>{elapsed} seconds</span>}
+
+          {(state === QuestionState.TimerSet ||
+            state === QuestionState.TimerExpired) && (
+            <button
+              className="rounded bg-blue-200 p-2 disabled:bg-slate-200"
+              onClick={() => {
+                setState(QuestionState.AnswerShown);
+              }}
+            >
+              <span>Show answer</span>
+            </button>
+          )}
+
+          {state === QuestionState.AnswerShown && (
+            <div className="flex flex-col gap-2">
+              <div>{question.answer}</div>
+              <div className="flex gap-1">
+                <button
+                  className="w-20 rounded p-2 bg-green-200"
+                  onClick={() => onAnswer(true)}
+                >
+                  Yes
+                </button>
+                <button
+                  className="w-20 rounded p-2 bg-red-200"
+                  onClick={() => onAnswer(false)}
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
   );
 }
